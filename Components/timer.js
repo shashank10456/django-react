852c8b98fe6ddf816de516d6e3dc3a03ae0638d5
shashank10456 <shashank10456@gmail.com>
2017-12-31 15:56:29 +0530

    first commit

---
+++ Components/timer.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+const Timer= (props)=>{
+    return(
+        <div>
+
+            <p>{props.sec}</p>
+        </div>
+    );
+};
+export default Timer;
--- index.js
+++ index.js
@@ -4,25 +4,42 @@ import ReactDom from 'react-dom';
 import QuestionList from './Components/questlist'
 import Textarea from './Components/textArea';
 import Question from "./Components/queDisplay";
+import Timer from "./Components/timer";
 class App extends Component{
     constructor(props){
         super(props);
         this.state={
             question:'-1',
-            list:[]
+            list:[],
+            seconds:3600,
+            mf:0
         };
+        this.tf=this.tf.bind(this);
         this.selectquestion=this.selectquestion.bind(this);
+        this.tf();
     }
     selectquestion(term){
         this.setState({question:term});
         console.log(this.state.question +" 1st console");
         console.log(this.state.question);
     }
+
+    tf(){
+
+            setInterval(() => {
+                {
+                    if (this.state.seconds > 0)
+                        this.setState({seconds: this.state.seconds - 1});
+                }
+            }, 1000);
+
+    }
     render(){
         return(
             <div>
                 <QuestionList sss={this.selectquestion}/>
                  <Question display={this.state.question}/>
+                <Timer td={this.tf()} sec={this.state.seconds}/>
                 <Textarea />
             </div>
     )
